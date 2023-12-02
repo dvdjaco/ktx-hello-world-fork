7010b8d0919ecf831be41f563229febbd65ee21f
David Jacovkis <dvdjaco@femprocomuns.coop>
2023-12-02 18:38:53 +0100

    Add healthckeck

--- hello_world.py
+++ hello_world.py
@@ -1,14 +1,16 @@
 # hello_world
 from flask import Flask
-import platform
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    system_info = f"System: {platform.system()} {platform.release()}"
     hello_world = "Kubeops :: Hello, world!"
     return hello_world
+
+@app.route('/health')
+def healthcheck():
+    return 'OK'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
